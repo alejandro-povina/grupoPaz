f5805c393fef4f6bf45f467a634d116800682f1d
Alejandro Poviña <alejandro.povina@gmail.com>
2021-08-09 20:53:00 -0300

    se agrego scroll to top a nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,37 +6,60 @@ import {Link ,NavLink ,withRouter } from 'react-router-dom';
 
 const Navigation = () => {
   const [expanded, setExpanded] = useState(false);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
 
     return (
 <Navbar className="nav container-fluid py-2 w-100 px-3" variant="dark" expand="lg" expanded={expanded}>
-  <Navbar.Brand className="d-flex flex-column align-items-center" as={Link} to="/" onClick={() => setTimeout(() => {setExpanded(false)}, 150)} >Grupo Paz <hr className="hrNav my-2" /></Navbar.Brand>
+  <Navbar.Brand 
+  className="d-flex flex-column align-items-center" 
+  as={Link} 
+  to="/" 
+  onClick={() => setTimeout(() => {
+    scrollToTop(); 
+    setExpanded(false)}, 150)}>
+      Grupo Paz 
+      <hr className="hrNav my-2" />
+      </Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto" >
       <NavLink 
-       onClick={() => setTimeout(() => {setExpanded(false)}, 150)}
+       onClick={() => setTimeout(() => {
+        scrollToTop(); 
+        setExpanded(false)}, 150)}
       exact={true} 
       className="nav-link" 
       to="/empresa">
         La Empresa
         </NavLink>
       <NavLink 
-      onClick={() => setTimeout(() => {setExpanded(false)}, 150)}
+      onClick={() => setTimeout(() => {
+        scrollToTop(); 
+        setExpanded(false)}, 150)}
       exact={true} 
       className="nav-link" 
       to="/contacto">
         Contacto
         </NavLink>
       <NavLink 
-      onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+      onClick={() => setTimeout(() => {
+        scrollToTop(); 
+        setExpanded(false)}, 150)} 
       exact={true} 
       className="nav-link" 
       to="/productos">
         Productos
         </NavLink>
       <NavLink 
-      onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+      onClick={() => setTimeout(() => {
+        scrollToTop(); 
+        setExpanded(false)}, 150)} 
       exact={true} 
       className="nav-link" 
       to="/">
@@ -44,21 +67,27 @@ const Navigation = () => {
         </NavLink>
       <NavDropdown title="Servicios" className="" id="basic-nav-dropdown">
         <NavDropdown.Item 
-        onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+        onClick={() => setTimeout(() => {
+          scrollToTop(); 
+          setExpanded(false)}, 150)} 
         className="text-dark  m-0"  
         as={Link} 
         to="/servicios/logistica">
           Logistica
           </NavDropdown.Item>
         <NavDropdown.Item 
-        onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+        onClick={() => setTimeout(() => {
+          scrollToTop(); 
+          setExpanded(false)}, 150)} 
         className="text-dark  m-0"  
         as={Link} 
         to="/servicios/almacenaje">
           Almacenaje
           </NavDropdown.Item>
         <NavDropdown.Item 
-        onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+        onClick={() => setTimeout(() => {
+          scrollToTop(); 
+          setExpanded(false)}, 150)} 
         className="text-dark  m-0"  
         as={Link} 
         to="/servicios/agro">
