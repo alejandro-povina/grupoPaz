02e1cd0908a002853651fc9ed5c8e6a1083e680e
Alejandro Poviña <alejandro.povina@gmail.com>
2021-08-09 20:29:02 -0300

    se agrego funcion para cerrar boton hamburguesa una vez hecho el click

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,69 @@
-import React from 'react';
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+import {React, useState} from 'react';
 import {Link ,NavLink ,withRouter } from 'react-router-dom';
 
 
 
 const Navigation = () => {
+  const [expanded, setExpanded] = useState(false);
+
+
     return (
-<Navbar className="nav container-fluid py-2 w-100 px-3" variant="dark" expand="lg">
-  <Navbar.Brand className="d-flex flex-column align-items-center" as={Link} to="/">Grupo Paz <hr className="hrNav my-2" /></Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+<Navbar className="nav container-fluid py-2 w-100 px-3" variant="dark" expand="lg" expanded={expanded}>
+  <Navbar.Brand className="d-flex flex-column align-items-center" as={Link} to="/" onClick={() => setTimeout(() => {setExpanded(false)}, 150)} >Grupo Paz <hr className="hrNav my-2" /></Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <NavLink exact={true} className="nav-link" to="/">Inicio</NavLink>
-      <NavLink exact={true} className="nav-link" to="/empresa">La Empresa</NavLink>
-      <NavLink exact={true} className="nav-link" to="/contacto">Contacto</NavLink>
-      <NavLink exact={true} className="nav-link" to="/productos">Productos</NavLink>
+      <NavLink 
+      onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+      exact={true} 
+      className="nav-link" 
+      to="/empresa">
+        La Empresa
+        </NavLink>
+      <NavLink 
+      onClick={() => setTimeout(() => {setExpanded(false)}, 150)}
+      exact={true} 
+      className="nav-link" 
+      to="/contacto">
+        Contacto
+        </NavLink>
+      <NavLink 
+      onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+      exact={true} 
+      className="nav-link" 
+      to="/productos">
+        Productos
+        </NavLink>
+      <NavLink 
+      onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+      exact={true} 
+      className="nav-link" 
+      to="/">
+        Inicio
+        </NavLink>
       <NavDropdown title="Servicios" className="" id="basic-nav-dropdown">
-        <NavDropdown.Item className="text-dark  m-0"  as={Link} to="/servicios/logistica">Logistica</NavDropdown.Item>
-        <NavDropdown.Item className="text-dark  m-0"  as={Link} to="/servicios/almacenaje">Almacenaje</NavDropdown.Item>
-        <NavDropdown.Item className="text-dark  m-0"  as={Link} to="/servicios/agro">Agro</NavDropdown.Item>
+        <NavDropdown.Item 
+        onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+        className="text-dark  m-0"  
+        as={Link} 
+        to="/servicios/logistica">
+          Logistica
+          </NavDropdown.Item>
+        <NavDropdown.Item 
+        onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+        className="text-dark  m-0"  
+        as={Link} 
+        to="/servicios/almacenaje">
+          Almacenaje
+          </NavDropdown.Item>
+        <NavDropdown.Item 
+        onClick={() => setTimeout(() => {setExpanded(false)}, 150)} 
+        className="text-dark  m-0"  
+        as={Link} 
+        to="/servicios/agro">
+          Agro
+          </NavDropdown.Item>
       </NavDropdown>
     </Nav>
   </Navbar.Collapse>  
