fa5e485a915f327ce21c08e44520a07a9aa1fd0f
Alejandro Poviña <alejandro.povina@gmail.com>
2021-08-09 17:37:23 -0300

    se ajusto codigo en barra de navegacion agregando Link de react router dom

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
-import { withRouter } from 'react-router-dom';
+import {Link ,NavLink ,withRouter } from 'react-router-dom';
 
 
 
 const Navigation = () => {
     return (
 <Navbar className="nav container-fluid py-2 w-100 px-3" variant="dark" expand="lg">
-  <Navbar.Brand className="d-flex flex-column align-items-center" href="/">Grupo Paz <hr className="hrNav my-2" /></Navbar.Brand>
+  <Navbar.Brand className="d-flex flex-column align-items-center" as={Link} to="/">Grupo Paz <hr className="hrNav my-2" /></Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="/">Inicio</Nav.Link>
-      <Nav.Link href="/empresa">La Empresa</Nav.Link>
-      <Nav.Link href="/contacto">Contacto</Nav.Link>
-      <Nav.Link href="/productos">Productos</Nav.Link>
+      <NavLink exact={true} className="nav-link" to="/">Inicio</NavLink>
+      <NavLink exact={true} className="nav-link" to="/empresa">La Empresa</NavLink>
+      <NavLink exact={true} className="nav-link" to="/contacto">Contacto</NavLink>
+      <NavLink exact={true} className="nav-link" to="/productos">Productos</NavLink>
       <NavDropdown title="Servicios" className="" id="basic-nav-dropdown">
-        <NavDropdown.Item className="text-dark m-0"  href="/servicios/logistica">Logistica</NavDropdown.Item>
-        <NavDropdown.Item className="text-dark m-0"  href="/servicios/almacenaje">Almacenaje</NavDropdown.Item>
-        <NavDropdown.Item className="text-dark m-0"  href="/servicios/agro">Agro</NavDropdown.Item>
+        <NavDropdown.Item className="text-dark  m-0"  as={Link} to="/servicios/logistica">Logistica</NavDropdown.Item>
+        <NavDropdown.Item className="text-dark  m-0"  as={Link} to="/servicios/almacenaje">Almacenaje</NavDropdown.Item>
+        <NavDropdown.Item className="text-dark  m-0"  as={Link} to="/servicios/agro">Agro</NavDropdown.Item>
       </NavDropdown>
     </Nav>
   </Navbar.Collapse>  
